c54ac37c9ab582dad847cd0990db3d5eeb3a2f6f
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-01 22:01:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,8 @@
-require "pry"
-
-#def oxford_comma(array)
- #   array.join(" and ")
-#end
-
-#what about iterating through the array and adding commas into the array before converting, and somwhow knowing when the last variable is so adding an and before it?
-
 def oxford_comma(array)
   if array.size == 1 
-    #binding.pry
     array.join
   elsif array.size == 2
     array.join(" and ")
-    #binding.pry
   else
     last_item = array.last
     array.pop
